155842b289c91db9c4a33bdd85fd77f855b24349
Saisindhuja Jampana <saisindhujajampana@SaisindhujasAir.attlocal.net>
2025-05-15 17:58:02 -0500

    modified instance profile name

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "servernode" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2-saisindhuja.name
+  iam_instance_profile   = aws_iam_instance_profile.saisindhuja.name
   connection {
     type        = "ssh"
     host        = self.public_ip
