f0b594409d8eee8ad1cd52ab4ef2a15e5804c708
Saisindhuja Jampana <saisindhujajampana@SaisindhujasAir.attlocal.net>
2025-05-15 17:54:36 -0500

    modified profile name

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "servernode" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2-saisindhuja.name
   connection {
     type        = "ssh"
     host        = self.public_ip
@@ -30,8 +30,8 @@ resource "aws_instance" "servernode" {
     "name" = "DeployVM"
   }
 }
-resource "aws_iam_instance_profile" "ec2-profile" {
-  name = "ec2-profile"
+resource "aws_iam_instance_profile" "saisindhuja" {
+  name = "saisindhuja"
   role = "ECR-LOGIN-AUTO"
 }
 resource "aws_security_group" "maingroup" {
