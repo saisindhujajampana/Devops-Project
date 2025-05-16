bc66da4d1f5a257e41dd928ad413cee4f96db872
Saisindhuja Jampana <saisindhujajampana@SaisindhujasAir.attlocal.net>
2025-05-16 10:56:37 -0500

    changing variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,20 @@
 variable "region" {
-    default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
+
 variable "public_key" {
-  
+  description = "Public key content for EC2 KeyPair"
+  type        = string
 }
+
 variable "private_key" {
-  
+  description = "Private key content for SSH connection"
+  type        = string
+  sensitive   = true
 }
+
 variable "key_name" {
-  
+  description = "Key pair name for EC2"
+  type        = string
 }
